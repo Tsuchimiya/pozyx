8badcae5669a2f99c9d7f9a51f0177cb4828ebed
Delia Loupias <loupiade@crim.ca>
2017-08-14 11:32:10 -0400

    fixed bug

--- ready_to_localize.py
+++ ready_to_localize.py
@@ -65,7 +65,7 @@ class ReadyToLocalize(object):
         else:
             self.printPublishErrorCode("positioning")
 
-    def store_position(position):
+    def store_position(self,position):
         """ Stores a position log into a file """
         if not (self.file is None):
             line = "T="+time.strftime("%H:%M:%S")+"/t X="+position.x+"\t Y="+position.y+"\t Z="+position.z
